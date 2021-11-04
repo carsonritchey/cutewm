537842c50a223c2450217ea4bfc5324d9cb7fa22
carsonritchey <ritcheycarson@gmail.com>
2021-11-04 16:56:42 -0500

    able to get display

---
+++ src/config.rs
@@ -0,0 +1,22 @@
+/*
+
+/*  stuff you can change */
+
+// character used to seperate workspace names
+pub const WORKSPACE_DELIMITER: char = ' ';
+
+// workspace names
+pub const WORKSPACE_NAMES: &str = "1 2 3 4 5 6"; 
+
+// whether or not to draw gaps when only one window is on screen
+pub const GAPS_ON_SINGLE: bool = false;
+
+// space between windows in pixels
+pub const GAPS: usize = 1;
+
+/* stuff you probably shouldn't change */
+
+pub const MIN_WINDOW_WIDTH: usize  = 10;
+pub const WIN_WINDOW_HEIGHT: usize = MIN_WINDOW_WIDTH;
+
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use x11::xlib::*;
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let mut arg0 = 0x0 as i8;
+    let display: *mut Display = unsafe { XOpenDisplay(&mut arg0) };
+
+    if display.is_null() {
+        println!("unable to get display\nexiting...");
+        std::process::exit(1);
+    }
 }
